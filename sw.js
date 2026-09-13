18d826b5bda8299ff1e05dab046182a00c528895
player13579 <230790503+player13579@users.noreply.github.com>
2026-09-13 15:05:12 +0900

    Publish male left dash v789

--- sw.js
+++ sw.js
@@ -5,10 +5,11 @@
 // v520 adds independent Natural Recovery resources and its persistent marker,
 // fixed Renki rewards, background resume repair, wall-default vector attacks,
 // and evidence-bound enemy Bot corpse investigation.
-const RUNTIME_RELEASE = "male-left-slow-v788";
-const CACHE_NAME = "dva-static-v788-male-left-slow";
+const RUNTIME_RELEASE = "male-left-dash-v789";
+const CACHE_NAME = "dva-static-v789-male-left-dash";
 const RUNTIME_RECOVERY_REVISION = "v591-movement-transport-resilience-1";
 const STATIC_ASSETS = [
+  "/assets/generated/male-left-dash-v789.png",
   "/assets/generated/male-left-slow-v788.png",
   "/assets/generated/philia-left-dash-v787.png",
   "/assets/generated/sophia-front-dash-v786.png",
